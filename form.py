784eeb74955543d6af87ce03997005760afe9348
Rob <roberthjhan@gmail.com>
2020-08-03 23:45:25 -0400

    Update worklog. Setup github for this project.

--- form.py
+++ form.py
@@ -2,14 +2,14 @@
 File containing Forms
 Author: Robert Han
 '''
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length, InputRequired, NoneOf
 
-
 class StockForm(FlaskForm):
     #field(label, validators)
-    ticker = StringField("Ticker", validators = [InputRequired(), Length(min = 1, max = 5), NoneOf(values = "~!@#$%^&*()_+`1234567890-=[]\{}|;':,./<>?)]")])
+    ticker = StringField("Ticker", validators = [InputRequired(),
+                                                 Length(min = 1, max = 5),
+                                                 NoneOf(values = "~!@#$%^&*()_+`1234567890-=[]\{}|;':,./<>?)]")])
     submit = SubmitField("Lookup")
-
-
